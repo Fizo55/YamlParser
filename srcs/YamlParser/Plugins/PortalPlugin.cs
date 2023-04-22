924c9245dbfa3345b61fd020fa86c62fe5596fd6
Fizo55 <hugomacees56000@gmail.com>
2023-04-23 00:51:48 +0200

    chore: cleanup portal plugin

--- srcs/YamlParser/Plugins/PortalPlugin.cs
+++ srcs/YamlParser/Plugins/PortalPlugin.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Options;
 using YamlDotNet.Serialization;
 using YamlParser.Core;
 using YamlParser.Entities;
 using YamlParser.Extensions;
 using YamlParser.Shared;
+using System.IO;
+using System.Linq;
 
 namespace YamlParser.Plugins
 {
@@ -19,72 +21,48 @@ namespace YamlParser.Plugins
         public void Run()
         {
             DateTime start = DateTime.Now;
-            Portal toSerialize = new();
             List<int> mapAlreadyDone = new();
-            int map = 0;
-            List<PortalAttributes> portals = new();
-            List<string> filteredPortalLines = $"{_configuration.BasePath}{_configuration.PacketName}".FilterLines(new string[] { "c_map ", "gp " });
-
-            // get each portals existing in packet.txt
-            foreach (string line in filteredPortalLines)
-            {
-                string[] parts = line.Split(' ');
-
-                if (parts.Length > 3 && parts[0] == "c_map")
+            List<PortalAttributes> portals = $"{_configuration.BasePath}{_configuration.PacketName}".FilterLines(new string[] { "c_map ", "gp " })
+                .SelectMany((line, index) =>
                 {
-                    map = int.Parse(parts[2]);
-                    continue;
-                }
-
-                if (parts.Length <= 4 || parts[0] != "gp") continue;
-
-                if (!File.Exists($"{_configuration.BasePath}{_configuration.BinaryMapFolder}/{map}") || !File.Exists($"{_configuration.BasePath}{_configuration.BinaryMapFolder}/{int.Parse(parts[3])}")) continue;
-
-                portals.Add(new PortalAttributes
+                    string[] parts = line.Split(' ');
+                    if (parts.Length > 3 && parts[0] == "c_map")
+                    {
+                        return new[] { (mapId: int.Parse(parts[2]), lineParts: parts) };
+                    }
+                    return Enumerable.Empty<(int, string[])>();
+                })
+                .Where(tuple => tuple.lineParts.Length > 4 && tuple.lineParts[0] == "gp" && File.Exists($"{_configuration.BasePath}{_configuration.BinaryMapFolder}/{tuple.mapId}") && File.Exists($"{_configuration.BasePath}{_configuration.BinaryMapFolder}/{int.Parse(tuple.lineParts[3])}"))
+                .Select(tuple => new PortalAttributes
                 {
-                    SourceMapId = map,
-                    SourceMapX = short.Parse(parts[1]),
-                    SourceMapY = short.Parse(parts[2]),
-                    DestinationMapId = int.Parse(parts[3]),
+                    SourceMapId = tuple.mapId,
+                    SourceMapX = short.Parse(tuple.lineParts[1]),
+                    SourceMapY = short.Parse(tuple.lineParts[2]),
+                    DestinationMapId = int.Parse(tuple.lineParts[3]),
                     DestinationMapX = 0,
                     DestinationMapY = 0,
-                    Type = short.Parse(parts[4])
-                });
-            }
+                    Type = short.Parse(tuple.lineParts[4])
+                })
+                .Distinct()
+                .ToList();
 
-            var temp = portals.Distinct();
-            portals = temp.ToList();
-
-            // foreach in all portals
             foreach (PortalAttributes portal in portals)
             {
                 if (mapAlreadyDone.Contains(portal.SourceMapId)) continue;
 
-                toSerialize = new();
-
-                // then get all portals of the same map
-                foreach (PortalAttributes specialPortal in portals.Where(s => s.SourceMapId == portal.SourceMapId))
+                Portal toSerialize = new() { Portals = new() };
+                portals.Where(s => s.SourceMapId == portal.SourceMapId).ToList().ForEach(specialPortal =>
                 {
                     PortalAttributes portal1 = portals.FirstOrDefault(s => s.SourceMapId == specialPortal.DestinationMapId);
-
-                    if (portal1 == null) continue;
+                    if (portal1 == null) return;
 
                     specialPortal.DestinationMapX = portal1.SourceMapX;
                     specialPortal.DestinationMapY = portal1.SourceMapY;
-
-                    if (toSerialize.Portals != null)
-                    {
-                        toSerialize.Portals.Add(specialPortal);
-                    }
-                    else
-                    {
-                        toSerialize.Portals = new() { specialPortal };
-                    }
-                }
+                    toSerialize.Portals.Add(specialPortal);
+                });
 
                 var serializer = new SerializerBuilder().DisableAliases().Build();
                 var yaml = serializer.Serialize(toSerialize);
-
                 _configuration.CreateFile($"portals_{portal.SourceMapId}", _configuration.PortalFolder, yaml);
                 mapAlreadyDone.Add(portal.SourceMapId);
             }
